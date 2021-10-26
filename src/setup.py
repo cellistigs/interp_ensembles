69768be3f55c5b777473861b7ac2c18de6fdbc3f
cellistigs <ta2507@columbia.edu>
2021-10-26 15:31:21 -0400

    added setup file.

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+
+loc = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(loc,"../","README.md"),"r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "interpensembles",
+        version = "0.0.1",
+        author = "Taiga Abe",
+        author_email = "ta2507@columbia.edu",
+        description = "Interpolation-ensembles", 
+        long_description = long_description,
+        long_description_content_type = "test/markdown", 
+        url = "https://github.com/cellistigs/interp-ensembles",
+        packages = setuptools.find_packages(),
+        include_package_data=True,
+        package_data={},
+        classifiers = [
+            "License :: OSI Approved :: MIT License"],
+        python_requires=">=3.6",
+        )
+
