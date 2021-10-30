55d6d6fa0cdc2df4e4194297fc222d4e6419d32e
cellistigs <ta2507@columbia.edu>
2021-10-29 21:26:35 -0400

    added basic parameter count check

--- tests/test_wide_basic.py
+++ tests/test_wide_basic.py
@@ -19,3 +19,16 @@ def test_wresnet():
     y = model(dummy_input.double())
     dot = make_dot(y[0].mean(),params = dict(model.named_parameters()))
     dot.render("test_mats/test_resnet18_2x",view = True)
+
+def test_paramcount():    
+    """Test that the wide resnet has approximately 4x the number of trainable parameters. 
+
+    """
+    def count_parameters(model):
+
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    model = resnets.resnet18().double()
+    widemodel = resnets.wideresnet18().double()
+    ## the parameter count should be dominated by square convolutions. Figure out a better way to count these later. 
+    assert count_parameters(model)*4/count_parameters(widemodel) -1 < 0.001
