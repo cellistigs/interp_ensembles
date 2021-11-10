866b5674efd98079362a9c96db0e1304279d204e
Ubuntu <ubuntu@ip-172-31-0-219.ec2.internal>
2021-11-10 23:30:50 +0000

    added functionality to write out results as softmax probabilities and labels to the results folder.

--- scripts/plot_rel_robustness.py
+++ scripts/plot_rel_robustness.py
@@ -4,58 +4,104 @@ import json
 from argparse import ArgumentParser
 
 import torch
+import numpy as np
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger, TensorBoardLogger
 
 from interpensembles.data import CIFAR10Data,CIFAR10_1Data
-from interpensembles.module import CIFAR10Module
+from interpensembles.module import CIFAR10Module,CIFAR10EnsembleModule,CIFAR10InterEnsembleModule
 
+modules = {"base":CIFAR10Module,
+        "ensemble":CIFAR10EnsembleModule,
+        "interpensemble":CIFAR10InterEnsembleModule}
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
 
 def main(args):
+    seed_everything(0)
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
 
-    if bool(args.download_weights):
-        CIFAR10Data.download_weights()
-    else:
-        seed_everything(0)
-        os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+    if args.logger == "wandb":
+        logger = WandbLogger(name=args.classifier, project="cifar10")
+    elif args.logger == "tensorboard":
+        logger = TensorBoardLogger("cifar10", name=args.classifier)
 
-        if args.logger == "wandb":
-            logger = WandbLogger(name=args.classifier, project="cifar10")
-        elif args.logger == "tensorboard":
-            logger = TensorBoardLogger("cifar10", name=args.classifier)
+    checkpoint = ModelCheckpoint(monitor="acc/val", mode="max", save_last=False)
 
-        checkpoint = ModelCheckpoint(monitor="acc/val", mode="max", save_last=False)
+    trainer = Trainer(
+        fast_dev_run=bool(args.dev),
+        logger=logger if not bool(args.dev + args.test_phase) else None,
+        gpus=-1,
+        deterministic=True,
+        weights_summary=None,
+        log_every_n_steps=1,
+        max_epochs=args.max_epochs,
+        checkpoint_callback=checkpoint,
+        precision=args.precision,
+    )
 
-        trainer = Trainer(
-            fast_dev_run=bool(args.dev),
-            logger=logger if not bool(args.dev + args.test_phase) else None,
-            gpus=-1,
-            deterministic=True,
-            weights_summary=None,
-            log_every_n_steps=1,
-            max_epochs=args.max_epochs,
-            checkpoint_callback=checkpoint,
-            precision=args.precision,
+    #model = CIFAR10Module(args)
+    if args.module == "base":
+        model = modules[args.module].load_from_checkpoint(checkpoint_path=args.checkpoint)
+    elif args.module == "ensemble":
+        model = modules[args.module].load_from_checkpoint(nb_models = args.nb_models,checkpoint_path=args.checkpoint)
+    elif args.module == "interpensemble":
+        model = modules[args.module].load_from_checkpoint(lamb = args.lamb,checkpoint_path=args.checkpoint)
+    cifar10data = CIFAR10Data(args)
+    cifar10_1data = CIFAR10_1Data(args,version =args.version)
+
+
+    if bool(args.pretrained):
+        state_dict = os.path.join(
+            "cifar10_models", "state_dicts", args.classifier + ".pt"
         )
+        model.model.load_state_dict(torch.load(state_dict))
 
-        #model = CIFAR10Module(args)
-        model = CIFAR10Module.load_from_checkpoint(checkpoint_path=args.checkpoint)
-        cifar10data = CIFAR10Data(args)
-        cifar10_1data = CIFAR10_1Data(args)
+    data = {"in_dist_acc":None,"out_dist_acc":None}
+    data["in_dist_acc"] = trainer.test(model, cifar10data.test_dataloader())[0]["acc/test"]
+    data["out_dist_acc"] = trainer.test(model, cifar10_1data.test_dataloader())[0]["acc/test"]
+
+    all_preds_ind = []
+    all_labels_ind = []
+    all_preds_ood = []
+    all_labels_ood = []
+
+    model.eval()
+    with torch.no_grad():
+        for idx,batch in enumerate(cifar10data.test_dataloader()):
+            ims = batch[0].to("cuda")
+            labels = batch[1].to("cuda")
+            pred,label = model.calibration((ims,labels))
+            ## to cpu
+            predarray = pred.cpu().numpy() ## 256x10
+            labelarray = label.cpu().numpy() ## 
+            all_preds_ind.append(predarray)
+            all_labels_ind.append(labelarray)
+        for idx,batch in enumerate(cifar10_1data.test_dataloader()):
+            ims = batch[0].to("cuda")
+            labels = batch[1].to("cuda")
+            pred,label = model.calibration((ims,labels))
+            ## to cpu
+            predarray = pred.cpu().numpy() ## 256x10
+            labelarray = label.cpu().numpy() ## 
+            all_preds_ood.append(predarray)
+            all_labels_ood.append(labelarray)
 
 
-        if bool(args.pretrained):
-            state_dict = os.path.join(
-                "cifar10_models", "state_dicts", args.classifier + ".pt"
-            )
-            model.model.load_state_dict(torch.load(state_dict))
+    results_dir = os.path.join(script_dir,"../results")
+    full_path = os.path.join(results_dir,"robust_results{}_{}_{}".format(datetime.datetime.now().strftime("%m-%d-%y_%H:%M.%S"),args.module,args.classifier))
+    np.save(full_path+"ind_preds",np.concatenate(all_preds_ind,axis = 0))
+    np.save(full_path+"ind_labels",np.concatenate(all_labels_ind,axis = 0))
+    np.save(full_path+"ood_preds",np.concatenate(all_preds_ood,axis = 0))
+    np.save(full_path+"ood_labels",np.concatenate(all_labels_ood,axis = 0))
+    ## write metadata
+    with open(full_path+"_meta.json","w") as f:
+        json.dump(vars(args),f)
 
-        data = {"in_dist_acc":None,"out_dist_acc":None}
-        data["in_dist_acc"] = trainer.test(model, cifar10data.test_dataloader())[0]["acc/test"]
-        data["out_dist_acc"] = trainer.test(model, cifar10_1data.test_dataloader())[0]["acc/test"]
-        with open("robust_results{}".format(datetime.datetime.now().strftime("%m-%d-%y_%H:%M.%S")),"w") as f:
-            json.dump(data,f)
+
+    #with open(os.path.join(results_dir,"robust_results{}_{}_{}".format(datetime.datetime.now().strftime("%m-%d-%y_%H:%M.%S"),args.module,args.classifier)),"w") as f:
+    #    json.dump(data,f)
 
 
 if __name__ == "__main__":
