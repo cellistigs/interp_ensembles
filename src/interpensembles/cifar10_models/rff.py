8ea4ce66d1ca311a2dc52f2fe921346c961150f3
Taiga Abe <taigaabe@dyn-160-39-153-209.dyn.columbia.edu>
2022-11-14 15:35:18 -0500

    updated rff model

--- src/interpensembles/cifar10_models/rff.py
+++ src/interpensembles/cifar10_models/rff.py
@@ -4,26 +4,28 @@ from torch import nn
 import numpy as np
 
 class RFF_Projection(nn.Module):
-    def __init__(self,input_size,project_size):
+    def __init__(self,input_size,project_size,sigma):
         """Create an rff projection
 
         """
         super().__init__()
-        self.weights = nn.init.normal_(torch.Tensor(project_size,input_size),1/np.sqrt(self.sigma*input_size))
-        self.offset = nn.init.uniform_(torch.Tensor(size_out),0,2*np.pi)
-        self.weights.requires_grad = False
-        self.offset.requires_grad = False
+        self.weights = nn.Parameter(torch.Tensor(project_size,input_size),requires_grad = False)
+        self.offset = nn.Parameter(torch.Tensor(project_size),requires_grad = False)
+        nn.init.normal_(self.weights,sigma,1/np.sqrt(sigma*input_size))
+        nn.init.uniform_(self.offset,0,2*np.pi)
         
     def forward(self,x):    
-        return torch.cos(torch.mm(x,self.weights.t())+self.offset)
+        return torch.cos(torch.mm(x,self.weights.T)+self.offset)
 
 class RFF(nn.Module): 
     """Random Fourier Features module.
 
     """
     def __init__(self,input_size,project_size,output_size,sigma = 1):
-        self.project = RFF_Projection(input_size,project_size) 
+        super().__init__()
+        self.project = RFF_Projection(input_size,project_size,sigma) 
         self.regress = nn.Linear(project_size,output_size) 
+        self.sigma = sigma
 
     def forward(self,x):    
         proj = self.project(x)
